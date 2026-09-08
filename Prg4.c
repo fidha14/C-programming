88341e4865471039692c6a08768e3da7614de683
fidha14 <26BST018@hicas.ac.in>
2026-09-08 20:18:08 +0530

    Prg4.c

---
+++ Prg4.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int factorial(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main()
+{
+    int n, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    result = factorial(n);
+
+    printf("Factorial = %d", result);
+
+    return 0;
+}
+Output:
+Enter a number: 5
+Factorial = 120
