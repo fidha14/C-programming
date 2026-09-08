bb3e99d138b7381eabba43c7212911df44694d61
fidha14 <26BST018@hicas.ac.in>
2026-09-08 20:14:46 +0530

    Prg2.c

---
+++ Pgm2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int a, b, result;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    result = add(a, b);
+
+    printf("Sum = %d", result);
+
+    return 0;
+}
+Output:
+Enter two numbers: 10 20
+Sum = 30
